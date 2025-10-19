a6ebb174894e762c622fd2fc9efe307f9a87342f
MORGAN_0913 <1makardaniil1@gmail.com>
2025-10-19 20:01:35 +0300

    Знакомство с WebSocket протоколом

--- websocket_users_client.py
+++ websocket_users_client.py
@@ -5,11 +5,11 @@ async def client():
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
         message = f"Привет, сервер!"
-        print(f"Сервер отправлено сообщение: {message}")
+        print(f"Сервер отправлено сообщение: {message}") #Пишем влог для себя
         await websocket.send(message)
 
         for _ in range(5):
             response = await websocket.recv()
-            print(f"Результат: {response}")
+            print(f"Результат: {response}") #Пишем влог для себя
 
 asyncio.run(client())
--- websocket_users_server.py
+++ websocket_users_server.py
@@ -4,13 +4,13 @@ from websockets import ServerConnection
 
 async def echo(websocket: ServerConnection):
     async for message in websocket:
-        print(f"Получено сообщение от пользователя: {message}")
+        print(f"Получено сообщение от пользователя: {message}") #Пишем влог для себя
         response = f"Сервер получил сообщение от клиента: {message}"
         for _ in range(5):
             await websocket.send(response)
 
 async def main():
-    print(f"WebSocketServer успешно запущен!")
+    print(f"WebSocketServer успешно запущен!") #Пишем влог для себя
     async with websockets.serve(echo, "localhost", 8765) as serever:
         await asyncio.Future()
 
